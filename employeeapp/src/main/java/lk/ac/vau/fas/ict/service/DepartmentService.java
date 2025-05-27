6b5dad74200e5bfd9d17af3f825032f35060aad8
Apsara2001 <apsaraduanji2001@gmail.com>
2025-05-27 23:18:11 +0530

    Fix errors

--- employeeapp/src/main/java/lk/ac/vau/fas/ict/service/DepartmentService.java
+++ employeeapp/src/main/java/lk/ac/vau/fas/ict/service/DepartmentService.java
@@ -3,6 +3,7 @@ package lk.ac.vau.fas.ict.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 
